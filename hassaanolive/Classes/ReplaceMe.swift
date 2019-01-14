b1888548fe6097b506723b7ba306733806d2905f
Hassaan Shakeel <Hassaan.Shakeel@docusign.com>
2019-01-14 14:46:31 -0800

    added bundle access file for uiimage

---
+++ hassaanolive/Classes/ReplaceMe.swift
@@ -0,0 +1,22 @@
+
+import UIKit
+
+extension UIImage {
+
+    public class func bundledImage(named: String) -> UIImage? {
+        let image = UIImage(named: named)
+        if image == nil {
+            let podBundle = Bundle(for: Olive.classForCoder())
+            let url = podBundle.url(forResource: "Assets", withExtension: "bundle")
+            let imageBundle = Bundle(url: url!)
+            return UIImage(named: named, in:imageBundle, compatibleWith: nil)
+        }
+        return image
+    }
+
+    public class var docusignLogo : UIImage {
+        return self.bundledImage(named: "tacocat")!
+    }
+
+}
+

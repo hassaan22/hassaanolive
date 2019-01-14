071581d8f9b05e7499eca1d27b56742f92c82365
Hassaan Shakeel <Hassaan.Shakeel@docusign.com>
2019-01-14 14:50:13 -0800

    fixed typy

--- hassaanolive/Classes/ReplaceMe.swift
+++ hassaanolive/Classes/ReplaceMe.swift
@@ -6,7 +6,7 @@ extension UIImage {
     public class func bundledImage(named: String) -> UIImage? {
         let image = UIImage(named: named)
         if image == nil {
-            let podBundle = Bundle(for: Olive.classForCoder())
+            let podBundle = Bundle(for: self.classForCoder())
             let url = podBundle.url(forResource: "Assets", withExtension: "bundle")
             let imageBundle = Bundle(url: url!)
             return UIImage(named: named, in:imageBundle, compatibleWith: nil)

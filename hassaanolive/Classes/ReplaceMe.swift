e95a5117910d11f28fa25f006d1ccf67442eb78a
Hassaan Shakeel <Hassaan.Shakeel@docusign.com>
2019-01-14 16:28:03 -0800

    fixed icons file

--- hassaanolive/Classes/ReplaceMe.swift
+++ hassaanolive/Classes/ReplaceMe.swift
@@ -6,16 +6,18 @@ public extension UIImage {
     public class func bundledImage(named: String) -> UIImage? {
         let image = UIImage(named: named)
         if image == nil {
-            let podBundle = Bundle(for: self.classForCoder())
-            let url = podBundle.url(forResource: "Assets", withExtension: "bundle")
-            let imageBundle = Bundle(url: url!)
-            return UIImage(named: named, in:imageBundle, compatibleWith: nil)
+            let podBundle = Bundle(for: OliveIcons.self)
+            return UIImage(named: named, in:podBundle, compatibleWith: nil)
         }
         return image
     }
 
+
+}
+
+public class OliveIcons {
     public class var docusignLogo : UIImage {
-        return self.bundledImage(named: "tacocat")!
+        return UIImage.bundledImage(named: "tacocat")!
     }
 
 }
